7295df686ae47a2a11117beb1288ae21ad172df0
irisida <20445648+irisida@users.noreply.github.com>
2020-10-18 20:22:26 +0100

    course assignement project

--- language_lessons/todo/02_03_custom_package/main.go
+++ language_lessons/todo/02_03_custom_package/main.go
@@ -1,7 +1,7 @@
 package main
 
-import "github.com/irisida/learngo/02_03_custom_package/printer"
+// import "github.com/irisida/lang/todo/02_03_custom_package/printer"
 
 func main() {
-	printer.Hello()
+	//printer.Hello()
 }
--- projects/05_http_interfaces/main.go
+++ projects/05_http_interfaces/main.go
@@ -2,9 +2,12 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"net/http"
 	"os"
 )
+
+type logWriter struct{}
 
 func main() {
 	fmt.Println("http interfaces example")
@@ -16,5 +19,27 @@ func main() {
 		os.Exit(1)
 	}
 
-	fmt.Println("response: ", resp.Body.Read)
+	/*
+		behind the scenes
+		data := make([]byte, 99999)
+		resp.Body.Read(data)
+		fmt.Println(string(data))
+	*/
+
+	/*
+		io.Copy implements takes params that
+		satisfy the writer interface: os.Stdout
+		satisfy the reader interface: resp.body
+	*/
+	//io.Copy(os.Stdout, resp.Body)
+
+	lw := logWriter{}
+
+	io.Copy(lw, resp.Body)
+}
+
+func (logWriter) Write(bs []byte) (int, error) {
+	fmt.Println(string(bs))
+	fmt.Printf("just wrote %v bytes. \n", len(bs))
+	return len(bs), nil
 }
