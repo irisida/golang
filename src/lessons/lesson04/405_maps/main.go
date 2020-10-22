373127a3087bb60db79b6dbf5c6c627261a33032
irisida <20445648+irisida@users.noreply.github.com>
2020-10-22 22:13:29 +0100

    more maps

--- src/lessons/lesson04/405_maps/main.go
+++ src/lessons/lesson04/405_maps/main.go
@@ -21,7 +21,19 @@ func main() {
 	fmt.Println(m)       // prints a map object
 	fmt.Println(m["go"]) // prints the value associated with the key
 
+	// is not present in the map and will therefore use the
+	// associated zero values for the types.
 	val, found := m["php"]
 	fmt.Println(val, found)
+
+	term := "js"
+
+	// run a conditional test to load the
+	// boolean (2nd value), then you can run the
+	// block that check against a boolean value
+	// running the block if true
+	if v, f := m[term]; f {
+		fmt.Println("conditional was found: ", v)
+	}
 
 }
